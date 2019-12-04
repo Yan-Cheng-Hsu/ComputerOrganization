77d3edc22c10a9d0923c94231ceb51fec135daec
unknown <Yan-Cheng-Hsu>
2019-12-04 16:09:21 +0800

    nothing important

---
+++ Test/Test.srcs/sources_1/new/Test.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/10/26 11:12:56
+// Design Name: 
+// Module Name: Test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Test(A,B,C);
+input A,B;
+output C;
+assign C = A&B;
+
+endmodule
